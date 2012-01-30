de53412f4bd7ae4bd8ac09dc520c2bc0a4a0bdd9
Nick Reed <reednj@missouri.edu>
2012-01-30 17:47:05 -0600

    LDAP login

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SitesUsage::Application.initialize!
+
+SitesUsage::Application.config.ldap = YAML.load_file("#{Rails.root}/config/ldap.yml")
--- config/initializers/ldap.rb
+++
@@ -1,5 +0,0 @@
-SitesUsage::Application.config.ldap = {:host => "ldap.missouri.edu", 
-                                        :port => 3286, 
-                                        :username => "mudoitsitesimage@col.missouri.edu", 
-                                        :password => "J0inM3!!",
-                                        :base => "DC=edu" }
