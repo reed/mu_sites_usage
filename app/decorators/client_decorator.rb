2be5f37fc715bc2d06a5011306265170b80784ef
Nick Reed <reednj77@gmail.com>
2012-02-23 01:51:19 -0600

    Switched details pane to table

--- app/decorators/client_decorator.rb
+++ app/decorators/client_decorator.rb
@@ -23,8 +23,8 @@ class ClientDecorator < ApplicationDecorator
   def user_info 
     if model.logged_in? && model.current_user.present?
       uid = h.content_tag :span, model.current_user, :class => "user_toggler uid"
-      ldap = Ldap.new
-      display_name = ldap.get_display_name(model.current_user)
+     # ldap = Ldap.new
+      display_name = "" #ldap.get_display_name(model.current_user)
       display_name = model.current_user if display_name.empty?
       u_name = h.content_tag :span, display_name, :class => "user_toggler display-name"
       data = uid + u_name
